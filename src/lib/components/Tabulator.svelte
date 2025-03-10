6851152f30016e68c1d34da19dcca2a3e225617b
Siarhei Ryzhko <yndarven@gmail.com>
2025-03-10 17:29:46 +0300

    feat(components): add tabulator wrapper

---
+++ src/lib/components/Tabulator.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import {
+		TabulatorFull as Tabulator,
+		type ColumnDefinition,
+		type Options as TabulatorOptions
+	} from 'tabulator-tables';
+	import 'tabulator-tables/dist/css/tabulator_materialize.min.css';
+
+	export interface TabulatorProps extends TabulatorOptions {
+		columns: ColumnDefinition[];
+	}
+
+	let { data, ...props }: TabulatorProps = $props();
+
+	let tableComponent: HTMLElement;
+
+	onMount(() => {
+		new Tabulator(tableComponent, {
+			data: data,
+			reactiveData: true,
+			...props
+		});
+	});
+</script>
+
+<div bind:this={tableComponent}></div>
