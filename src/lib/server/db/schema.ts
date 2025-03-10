a0688616ce063023baf33b5201f21084b4801349
Siarhei Ryzhko <yndarven@gmail.com>
2025-03-10 17:30:07 +0300

    fix(db): simplify schema for development

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -7,48 +7,12 @@ export const user = sqliteTable('user', {
 });
 
 export const graduate = sqliteTable('graduate', {
-	id: integer('id').primaryKey(),
-	fullName: text().notNull(),
-	// address: text().notNull(),
-	// phoneNumber: text(),
-	// spec: text(),
-	// specCode: text(),
-	// faculty: text(),
-	// formEducation: text(),
-	// educationLevel: text(),
-	// yearOfGraduation: integer(),
-	servesArmy: integer().default(0)
-
-	// // Info about distribution
-	// isDistributed: integer(),
-	// profiling: text(),
-	// companyName: text(),
-	// basedNameCompany: text(),
-	// requestOfDistribution: text(),
-	// requestOfDistributionIP: text(),
-	// targetAgreement: text(),
-	// olympiadParticipance: text(),
-	// arrivalConfirmation: text(),
-	// jobTitle: text(),
-	// working: integer(),
-	// servesArmy: integer(),
-	// onMaternityLeave: text(),
-	// worked: text(),
-	// letterDate: text(),
-	// periodOfCompulsoryService: text(),
-	// selfCare: text(),
-	//
-	// // Info about reassignment
-	// isReassigned: integer(),
-	// newCompanyName: text(),
-	//
-	// // Info about unemployed state
-	// isUnemployed: integer(),
-	// distributionRefReturned: text(),
-	// companyNotified: text(),
-	// notificationDate: text(),
-	// paysTutitionFee: text(),
-	// notes: text(),
-	// consolidation: text()
-	// // conscriptionEmployment: text(), TODO: tf is this
+	id: integer('id').primaryKey().notNull(),
+	fullName: text().notNull().unique(),
+	address: text(),
+	servesArmy: integer({ mode: 'boolean' }).default(false),
+	graduationDate: text().$type<Date>(),
+	isDistributed: integer({ mode: 'boolean' }).default(false),
+	isRedistributed: integer({ mode: 'boolean' }).default(false),
+	isUnemployed: integer({ mode: 'boolean' }).default(false)
 });
